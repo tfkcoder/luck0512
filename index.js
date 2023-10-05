1b8c68a46a7870afe2fe6d169adfe6f7be7b3c43
Luciano Jackson <102759674+tfkcodes@users.noreply.github.com>
2023-10-06 01:10:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+const fs = require('fs');
+const axios = require('axios');
+const { promisify } = require('util');
+const writeFileAsync = promisify(fs.writeFile);
+
+async function generateGitHubStats() {
+  try {
+    // Make API requests to fetch GitHub statistics
+    const response = await axios.get('https://api.github.com/users/tfkcodes');
+    const userData = response.data;
+
+    // Customize the statistics you want to display
+    const stats = {
+      name: userData.name,
+      username: userData.login,
+      followers: userData.followers,
+      following: userData.following,
+      stars: userData.public_repos,
+      contributions: userData.contributions,
+    };
+
+    // Generate the markdown content for your README
+    const markdownContent = `
+      ## GitHub Stats
+
+      - Name: ${stats.name}
+      - Username: ${stats.username}
+      - Followers: ${stats.followers}
+      - Following: ${stats.following}
+      - Stars: ${stats.stars}
+      - Contributions: ${stats.contributions}
+    `;
+
+    // Write the markdown content to the README.md file
+    await writeFileAsync('README.md', markdownContent);
+
+    console.log('GitHub Stats updated successfully!');
+  } catch (error) {
+    console.error('Error updating GitHub Stats:', error.message);
+  }
+}
+
+generateGitHubStats();
 
