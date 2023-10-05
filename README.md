eb19cf6b0d2511cce8933096a1702df2f243734c
tfkcoder <lucianojackson048@gmail.com>
2023-10-06 01:33:17 +0300

    set up profile

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 
       # Hi there! 👋
 
-      Welcome to my GitHub profile! I'm passionate about coding and open source.
+      Welcome to my GitHub profile! I'm passionate about coding and ui/ux.
 
       ## GitHub Stats
 
       <!-- GitHub Stats Card -->
-      ![GitHub Stats Card](https://github-readme-stats.vercel.app/api?username=Yourusername)
+      ![GitHub Stats Card](https://github-readme-stats.vercel.app/api?username=undefined)
 
       <!-- Additional Custom Cards -->
       <!-- Insert more cards here for additional statistics -->
--- index.js
+++ index.js
@@ -17,12 +17,12 @@ async function generateGitHubStats() {
     const markdownContent = `
       # Hi there! 👋
 
-      Welcome to my GitHub profile! I'm passionate about coding and open source.
+      Welcome to my GitHub profile! I'm passionate about coding and ui/ux.
 
       ## GitHub Stats
 
       <!-- GitHub Stats Card -->
-      ![GitHub Stats Card](https://github-readme-stats.vercel.app/api?username=${stats.username})
+      ![GitHub Stats Card](https://github-readme-stats.vercel.app/api?username=${stats.tfkcodes})
 
       <!-- Additional Custom Cards -->
       <!-- Insert more cards here for additional statistics -->
